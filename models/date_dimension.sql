e1ca36d6b4a517167de32caf5550788e5f34dcce
Auren Bradley MOYO Kamdem <auren.moyo@devoteam.com>
2026-09-17 10:15:27 +0000

    fix: date_dimension

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -1,27 +1,12 @@
 WITH CTE AS (
     select 
-    TO_TIMESTAMP(STARTED_AT) AS STARTED_AT,
-    DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
-    HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
-
-    CASE 
-    WHEN dayname(TO_TIMESTAMP(STARTED_AT)) IN ('sat', 'sun')
-    THEN 'WEEKEND'
-    ELSE 'BUSINESSDAY'
-    END AS DAY_TYPE,
-
-    CASE WHEN month(TO_TIMESTAMP(STARTED_AT)) IN (12,1,2)
-        THEN 'SPRING'
-        WHEN month(TO_TIMESTAMP(STARTED_AT)) IN (6,7,8)
-        THEN 'SUMMER'
-        ELSE 'AUUMN'
-        END AS STATION_OF_YEAR
-
-    from
-    {{ source('demo', 'bike') }}
+        TO_TIMESTAMP(STARTED_AT) AS STARTED_AT,
+        DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
+        HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
+        {{ day_type('STARTED_AT') }} AS DAY_TYPE,
+        {{ get_season('STARTED_AT') }} AS STATION_OF_YEAR
+    from {{ source('demo', 'bike') }}
     WHERE STARTED_AT != 'started_at'
 )
 
-select
-*
-from CTE
+select * from CTE
